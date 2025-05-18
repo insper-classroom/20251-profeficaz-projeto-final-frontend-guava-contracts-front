b79cdb27696eb3ad2f9aac98940ecda796a62d8e
matheusluciano99 <matheusluciano99@gmail.com>
2025-05-18 00:34:57 -0300

    alterando rota

--- src/pages/Categoria.jsx
+++ src/pages/Categoria.jsx
@@ -17,7 +17,7 @@ function Categoria () {
 
 
   useEffect(() => {
-    axios.get(`http://127.0.0.1:5000/servico/categoria/${id}`)
+    axios.get(`http://127.0.0.1:5000/categoria/servico/${id}`)
       .then((response) => {
         
         setServicos(response.data)
